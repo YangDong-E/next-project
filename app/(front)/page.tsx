dbbabfa5f5330e588d6f642c68efbfd50f047151
YangDong-E <ehddh125@gmail.com>
2024-04-25 18:43:10 +0900

    24.04.25 - Next-Asia(데이터 불러오기)

--- app/(front)/page.tsx
+++ app/(front)/page.tsx
@@ -1,7 +1,16 @@
+import ProductItem from '@/components/products/ProductItem'
+import data from '@/lib/data'
+
 export default function Home() {
-  return (
-    <>
-      <h2 className="text-2xl py-2">Latest Product</h2>
-    </>
-  )
+    return (
+        <>
+            <h2 className="text-2xl py-2">Latest Product</h2>
+            <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+                {/* data(파일). products(불러올데이터).map(함수)로 뿌려주기 */}
+                {data.products.map((product) => (
+                    <ProductItem key={product.slug} product={product} />
+                ))}
+            </div>
+        </>
+    )
 }
