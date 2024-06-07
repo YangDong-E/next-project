b7153b354e5429c396a848d9077cd003cf8be1dc
YangDong-E <ehddh125@gmail.com>
2024-06-07 20:42:21 +0900

    24.06.06 - Next-Asia(재고관리 해결중- OrderModel의 qty를 불러오기 실패)

--- app/api/orders/route.ts
+++ app/api/orders/route.ts
@@ -35,12 +35,20 @@ export const POST = auth(async (req: any) => {
 
         // CountInStock 찾기? <- POST로 보낼때 ProductModel의 CountInStock 와 OrderModel의 qty를 빼서 ProductModel의 CountInStock를 업데이트
         // 이 부분에서 ProductModel.CountInStock 와 OrderModel.qty 불러와야함
+        // 현재 ProductMdel.CountInStock는 불러왔지만 OrderModel.qty는 불러오지 못함
 
         const dbProductCount = await ProductModel.find(
             {
                 _id: { $in: payload.items.map((x: { _id: string }) => x._id) },
             },
             'countInStock'
+        )
+
+        const dbProductQty = await OrderModel.find(
+            {
+                _id: { $in: payload.items.map((x: { _id: string }) => x._id) },
+            },
+            'qty'
         )
 
         const dbProductPrices = await ProductModel.find(
@@ -53,9 +61,8 @@ export const POST = auth(async (req: any) => {
             ...x,
             product: x._id,
             price: dbProductPrices.find((x) => x._id === x._id).price,
-            countInStock:
-                dbProductCount.find((x) => x._id === x._id).countInStock -
-                payload.items.qty,
+            countInStock: dbProductCount.find((x) => x._id === x._id)
+                .countInStock,
             _id: undefined,
         }))
         const { itemsPrice, taxPrice, shippingPrice, totalPrice } =
